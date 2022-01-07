0115abc3b62b66f4b7990d0dbcf2ac3c18a0c5f1
Augusto Amaro <augustoamaro@icloud.com>
2022-01-07 12:36:45 -0300

    Added components examples

--- react-guide/src/components/ExpenseItem.js
+++ react-guide/src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
-import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import './styles/ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-    const month = props.date.toLocaleString('pt-BR', { month: 'long' });
-    const year = props.date.getFullYear();
-
     return (
         <div className="expense-item">
-            <div>
-                <div>{day}</div>
-                <div>{month}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
